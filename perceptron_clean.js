343959ac84224cf205907e772cc559fb0b7c37b8
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2018-01-10 15:20:28 +0100

    Update perceptron_clean.js

--- perceptron_clean.js
+++ perceptron_clean.js
@@ -1,17 +1,21 @@
-function Perceptron(numInputs) {
-  this.numInputs = numInputs;
-  this.weights = [];
-  this.c = 0.1;
-  this.bias = 1;  // not used
-  // init random weights
-  for (var i = 0; i < this.numInputs; i++) {
-    this.weights.push(random(-1, 1));
+class Perceptron {
+  constructor(numInputs) {
+    this.numInputs = numInputs;
+    this.weights = [];
+    this.c = 0.1;
+    this.bias = 1;  // not used
+    
+    // init random weights
+    for (let i = 0; i < this.numInputs; i++) {
+      this.weights.push(random(-1, 1));
+    }
   }
 
-  this.feedForward = function(inputs) {
+
+  feedForward(inputs) {
     // sum weighted inputs
     sumWeightedInputs = 0;
-    for (var i = 0; i < this.numInputs; i++) {
+    for (let i = 0; i < this.numInputs; i++) {
       sumWeightedInputs += inputs[i] * this.weights[i];
     }
     // activation
@@ -22,13 +26,15 @@ function Perceptron(numInputs) {
     }
   }
 
-  this.train = function(inputs, known) {
+
+  train(inputs, known) {
     // get guess and error
-    var guess = this.feedForward(inputs)
-    var error = known - guess;
+    let guess = this.feedForward(inputs)
+    let error = known - guess;
     // adjust weights
-    for (var i = 0; i < this.numInputs; i++) {
+    for (let i = 0; i < this.numInputs; i++) {
       this.weights[i] += error * inputs[i] * this.c;
     }
   }
+  
 }
