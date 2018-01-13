521c15b207151a42b40144e210d898816b96f7f9
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2018-01-13 19:48:19 +0100

    Update matrix_class.js

--- matrix_class.js
+++ matrix_class.js
@@ -1,146 +1,152 @@
 class Matrix {
-  constructor(rows, cols) {
-    this.rows = rows;
-    this.cols = cols;
-    this.matrix = [];
-    this.init();
-  }
-
-
-  // initialize this matrix by setting all values to a random number
-  init() {
-    for (let i = 0; i < this.rows; i++) {
-      let rw = [];
-      for (let j = 0; j < this.cols; j++) {
-        rw.push(Math.floor(Math.random() * 5));
-      }
-      this.matrix.push(rw);
+    constructor(rows, cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.data = [];
+        this.init();
     }
-  }
 
 
-  // return row n of this matrix
-  getRow(n) {
-    return this.matrix[n];
-  }
-
-
-  // return column n of this matrix
-  getCol(n) {
-    let c = [];
-		for (let i = 0; i < this.rows; i++) {
-			c.push(this.matrix[i][n]);
-		}
-		return c;
-  }
-
-
-  // return the dot product between two arrays
-  dotArr(a, b) {
-    let sum = 0;
-    for (let i = 0; i < a.length; i++) {
-      sum += a[i] * b[i];
-    }
-    return sum;
-  }
-
-
-  // perform a dot product between this matrix and other matrix
-  // the columns of this matrix should equal the rows of the other matrix for the dot product to work
-  dot(other) {
-    if (this.cols != other.rows) {
-      return false
-    }
-    let newMatrix = []
-    for (let i = 0; i < this.rows; i++) {
-      let newRow = [];
-      for (let j = 0; j < other.cols; j++) {
-        let dotprod = this.dotArr(this.getRow(i), other.getCol(j));
-        newRow.push(dotprod);
-      }
-      newMatrix.push(newRow);
-    }
-    this.matrix = newMatrix;
-  }
-
-
-  // multiply by a scalar or by another matrix
-  // if other is a matrix multiply each element of this matrix with the corresponding element of other matrix
-  // if other is not a matrix multiply each element with other
-  mult(other) {
-    if (other instanceof Matrix) {
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] *= other.matrix[i][j];
+    // initialize this data by setting all values to a random number
+    init() {
+        for (let i = 0; i < this.rows; i++) {
+            let rw = [];
+            for (let j = 0; j < this.cols; j++) {
+                rw.push(Math.floor(Math.random() * 5));
+            }
+            this.data.push(rw);
         }
-      }  
-    } else {
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] *= other;
-        }
-      }
     }
-  }
 
 
-  // turn the columns of this matrix into the rows and the rows into the columns
-  transpose() {
-    let newMatrix = [];
-    for (let i = 0; i < this.cols; i++) {
-      newMatrix.push(this.getCol(i));
+    // return row n of this matrix
+    getRow(n) {
+        return this.data[n];
     }
-    this.matrix = newMatrix;
-  }
 
 
-  // adds a number to all the elements of this matrix, or ads another matrix to this matrix
-  // adding another matrix to this matrix only works if the two matrices have the same dimensions
-  add(n) {
-    if (n instanceof Matrix) {
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] += n.matrix[i][j];
+    // return column n of this matrix
+    getCol(n) {
+        let c = [];
+        for (let i = 0; i < this.rows; i++) {
+            c.push(this.data[i][n]);
         }
-      }
-    } else {
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] += n;
-        }
-      }
+        return c;
     }
-  }
 
 
-  // subtracts a number from all the elements of this matrix, or subtracts another matrix from this matrix
-  // subtracting another matrix from this matrix only works if the two matrices have the same dimensions
-  sub(n) {
-    if (n instanceof Matrix) {
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] -= n.matrix[i][j];
+    // return the dot product between two arrays
+    dotArr(a, b) {
+        let sum = 0;
+        for (let i = 0; i < a.length; i++) {
+            sum += a[i] * b[i];
         }
-      }
-    } else {
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          this.matrix[i][j] -= n;
+        return sum;
+    }
+
+
+    // perform a dot product between this matrix and another matrix
+    // the columns of this matrix should equal the rows of the other matrix for the dot product to work
+    dot(other) {
+        if (this.cols != other.rows) {
+            return false
         }
-      }
+        let newdata = []
+        for (let i = 0; i < this.rows; i++) {
+            let newRow = [];
+            for (let j = 0; j < other.cols; j++) {
+                let dotprod = this.dotArr(this.getRow(i), other.getCol(j));
+                newRow.push(dotprod);
+            }
+            newdata.push(newRow);
+        }
+        this.data = newdata;
     }
-  }
 
 
-  // returns a flattend array of this.matrix
-  flat() {
-    let flatMatrix = [];
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        flatMatrix.push(this.matrix[i][j]);
-      }
+    // multiply by a scalar or by another matrix
+    // if other is a matrix multiply each element of this matrix with the corresponding element of other matrix
+    // if other is not a matrix multiply each element with other
+    mult(other) {
+        if (other instanceof data) {
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    this.data[i][j] *= other.data[i][j];
+                }
+            }
+        } else {
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    this.data[i][j] *= other;
+                }
+            }
+        }
     }
-    return flatMatrix;
-  }
-  
+
+
+    // turn the columns of this matrix into the rows and the rows into the columns
+    static transpose(other) {
+        let newdata = [];
+        for (let i = 0; i < other.cols; i++) {
+            newdata.push(other.getCol(i));
+        }
+        return newdata;
+    }
+
+
+    // adds a number to all the elements of this matrix, or ads another matrix to this data
+    // adding another matrix to this matrix only works if the two matrices have the same dimensions
+    add(n) {
+        if (n instanceof data) {
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.data[i].length; j++) {
+                    this.data[i][j] += n.data[i][j];
+                }
+            }
+        } else {
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.data[i].length; j++) {
+                    this.data[i][j] += n;
+                }
+            }
+        }
+    }
+
+
+    // subtracts a number from all the elements of this matrix, or subtracts another matrix from this matrix
+    // subtracting another matrix from this matrix only works if the two matrices have the same dimensions
+    sub(n) {
+        if (n instanceof data) {
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.data[i].length; j++) {
+                    this.data[i][j] -= n.data[i][j];
+                }
+            }
+        } else {
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.data[i].length; j++) {
+                    this.data[i][j] -= n;
+                }
+            }
+        }
+    }
+
+
+    // returns a flattend array of this matrix
+    flat() {
+        let flatdata = [];
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                flatdata.push(this.data[i][j]);
+            }
+        }
+        return flatdata;
+    }
+
+
+    // prints this.data to the console
+    print() {
+        console.log(this.data);
+    }
+
 }
